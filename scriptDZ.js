484d4aee3ee017c11fdccf53faf5ee3f10899def
Elizaveta Ilienko <ea_fomina_2003@mail.ru>
2026-06-28 11:20:42 +0700

    hw-5.js

--- scriptDZ.js
+++ scriptDZ.js
@@ -33,10 +33,50 @@ if (d > 100 || e > 100) {
 // Вариант 1 // 
 let a = '2';
 let b = '3';
-alert(Number(2)+Number(3))
+alert(Number(2) + Number(3))
 
 
 // Вариант 2 // 
 let a = '2';
 let b = '3';
 alert(+2 + +3)
+
+////// Задание 5 ////// 
+//  Создаем переменную с номером месяца 12
+let monthNumber = 12;
+
+//  Проверяем условие, при котором программа НЕ должна выполняться
+if (monthNumber > 13) {
+    console.log("Программа не выполняется, так как номер месяца больше 13");
+} else {
+    // 3. Если всё в порядке, определяем сезон через switch
+    let season;
+
+    switch (monthNumber) {
+        case 12:
+        case 1:
+        case 2:
+            season = "зима";
+            break;
+        case 3:
+        case 4:
+        case 5:
+            season = "весна";
+            break;
+        case 6:
+        case 7:
+        case 8:
+            season = "лето";
+            break;
+        case 9:
+        case 10:
+        case 11:
+            season = "осень";
+            break;
+        default:
+            season = "неизвестный сезон";
+    }
+
+    // 4. Выводим результат в консоль
+    console.log(monthNumber + "-й месяц принадлежит к сезону «" + season + "»");
+}
