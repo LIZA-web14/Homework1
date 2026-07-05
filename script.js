240cc63f38b199b5312e46617370583c5671e821
Elizaveta Ilienko <ea_fomina_2003@mail.ru>
2026-07-05 14:03:42 +0700

    hw-циклы

--- script.js
+++ script.js
@@ -1,81 +1,81 @@
-////// Задание 1 //////
+// ////// Задание 1 //////
 
-//  Создаем переменную a и записываем значение 10
-let a = 10;
-// Выводим значение (10) на экран
-alert(a);
-// Записываем в эту же переменную значение 20
-a = 20;
-// Выводим обновленное значение (20) на экран
-alert(20);
+// //  Создаем переменную a и записываем значение 10
+// let a = 10;
+// // Выводим значение (10) на экран
+// alert(a);
+// // Записываем в эту же переменную значение 20
+// a = 20;
+// // Выводим обновленное значение (20) на экран
+// alert(20);
 
-////// Задание 2 //////
+// ////// Задание 2 //////
 
-let iphoneFirstReleaseYear = 2007;
-alert(iphoneFirstReleaseYear);
+// let iphoneFirstReleaseYear = 2007;
+// alert(iphoneFirstReleaseYear);
 
-////// Задание 3 //////
+// ////// Задание 3 //////
 
-let jsCreatorName = "Брендан Эйх";
-alert(jsCreatorName);
+// let jsCreatorName = "Брендан Эйх";
+// alert(jsCreatorName);
 
-////// Задание 4 //////
+// ////// Задание 4 //////
 
-let A = 10;
+// let A = 10;
 
-let b = 2;
+// let b = 2;
 
-let sum = a + b; 
-alert(sum);
+// let sum = a + b; 
+// alert(sum);
 
-let difference = A - b; 
-alert(difference);
+// let difference = A - b; 
+// alert(difference);
 
-let product = A * b; 
-alert(product);
+// let product = A * b; 
+// alert(product);
 
-let quotient = A / b; 
-alert(quotient);
+// let quotient = A / b; 
+// alert(quotient);
 
-////// Задание 5 //////
+// ////// Задание 5 //////
 
-let result  = 2 ** 5;
-alert (result);
+// let result  = 2 ** 5;
+// alert (result);
 
-////// Задание 6 //////
+// ////// Задание 6 //////
 
-let variablesA = 9;
-let variablesB = 2;
-let remainder = variablesA % variablesB; 
-alert (remainder);
+// let variablesA = 9;
+// let variablesB = 2;
+// let remainder = variablesA % variablesB; 
+// alert (remainder);
 
-////// Задание 7 //////
+// ////// Задание 7 //////
 
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num++;
-num--;
-alert(num);
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num++;
+// num--;
+// alert(num);
 
-////// Задание 8 //////
+// ////// Задание 8 //////
 
-let age = prompt("Сколько вам лет?");
+// let age = prompt("Сколько вам лет?");
 
-alert(`Вам ${age} лет`);
+// alert(`Вам ${age} лет`);
 
-////// Задание 9 //////
+// ////// Задание 9 //////
 
-let user = {
-    name: " Елизавета",  // Строковое значение (String) — пишется в кавычках
-    age: 23,          // Числовое значение (Number) — пишется без кавычек
-    isAdmin: true     // Булево значение (Boolean) — пишется без кавычек (может быть только true или false)
-};
-////// Задание 10 //////
+// let user = {
+//     name: " Елизавета",  // Строковое значение (String) — пишется в кавычках
+//     age: 23,          // Числовое значение (Number) — пишется без кавычек
+//     isAdmin: true     // Булево значение (Boolean) — пишется без кавычек (может быть только true или false)
+// };
+// ////// Задание 10 //////
 
-let userName = prompt("Как Вас зовут?");
-alert (`Привет, ${userName}!`)
+// let userName = prompt("Как Вас зовут?");
+// alert (`Привет, ${userName}!`)
 
 
